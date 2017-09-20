981d8a92a5ccfc385665f49fb36d196ed540d243
Christopher Dimoulis <christopherjamesd@gmail.com>
2017-09-20 14:30:54 -0500

    Moved build to lib folder. Added build script

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const server = require('./src/server');
-const build = require('./build');
 
-const option = process.argv[2];
-
-if (option == 'server')
-  server.start();
-else
-  build();
+server.start();
 
 
-const fs = require('fs');
+// const fs = require('fs');
 
 // var watcher = fs.watch('./src/test.js', {encoding: 'utf-8'}, (eventType, filename) => {
 //   console.log('event', eventType, filename);
