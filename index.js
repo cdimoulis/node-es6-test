0a7201c9cd0d19cd228e44f372387ea1923bdcb1
Christopher Dimoulis <christopherjamesd@gmail.com>
2017-09-25 11:56:57 -0500

    Added file serving, route, request callback options, yarg option parser.

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const Server = require('./lib/server');
+const build = require('./lib/build');
+const yargs = require('yargs');
 
-const server = new Server();
+// Set the options
+setupOptions();
+
+let argv = yargs.argv;
+
+// If help then show and exit
+if (argv.help) {
+  yargs.showHelp();
+  process.exit(0);
+}
+
+
+const server = new Server({
+  on_request: onRequest,
+  port: argv.port,
+});
+
 server.start();
 
+function onRequest(req) {
+  // Build if request is for js
+  if (req.url === '/public/static/app.bundle.js')
+    build();
+};
 
-// const fs = require('fs');
 
-// var watcher = fs.watch('./src/test.js', {encoding: 'utf-8'}, (eventType, filename) => {
-//   console.log('event', eventType, filename);
-//   watcher.close();
-// });
+// Setup the yargs options
+function setupOptions() {
+  yargs
+    // Help option
+    .option('help', {
+      describe: 'Help documentation',
+      alias: 'h',
+    })
+    // Port Option
+    .option('port',{
+      describe: 'Port to bind on',
+      alias: 'p',
+      default: 3000,
+    });
+};
